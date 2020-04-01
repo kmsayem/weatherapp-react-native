f5618b0d0fac9ec4dc2a016610f67965f5a212e1
km sayem <sayem312518@gmail.com>
2020-04-01 22:06:22 +0600

    requestGeolocationPermission

--- app/screen/Home.js
+++ app/screen/Home.js
@@ -5,7 +5,8 @@ import {
   FlatList,
   StyleSheet,
   TouchableOpacity,
-  AppState
+  AppState,
+  PermissionsAndroid
 } from 'react-native';
 import { connect } from 'react-redux';
 import action from './../store/actions';
@@ -14,7 +15,6 @@ import Geolocation from '@react-native-community/geolocation';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { kelvinToCelsius } from '../Lib';
 import appConfig from '../appConfig';
-import Notification from '../Notification';
 
 const { 
   get_cities_list,
@@ -29,17 +29,56 @@ const HomeScreen = ({
 }) => {
   
   useEffect(()=> {
-    AppState.addEventListener('change', handleChange); 
+
+    PushNotification.configure({
+      onNotification: function(notification) {
+        console.log("NOTIFICATION:", notification);
+      },
+    });
+
+    AppState.addEventListener('change', handleChange);
+
     get_cities_list();
     return () => {
       AppState.removeEventListener('change', handleChange);  
     }
   },[]);
 
-  const handleChange = (newState) => {
-    if (newState !== "active") {
-      getTemperatureByGeolocation();
+  requestGeolocationPermission = async calback => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Weather App',
+          message: 'App needs access to your location',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        // this.setWallpaper();
+        return calback(true);
+      } else {
+        Alert.alert(
+          'Permission Denied!',
+          'You need to access your location',
+        );
+        return calback(false);
+      }
+    } catch (err) {
+      // console.warn(err);
+      return calback(false);
     }
+  };
+
+  
+  const handleChange = (newState) => {
+    requestGeolocationPermission(check => {
+      if (check == true && newState !== "active") {
+        getTemperatureByGeolocation();
+      }
+    });
   }
 
   const getTemperatureByGeolocation = () => {
@@ -99,7 +138,6 @@ const HomeScreen = ({
         renderItem={({ item, index}) => <Item item={item} key={index}/>}
         keyExtractor = { (item, index) => index.toString() }
       />
-      <Notification/>
     </SafeAreaView>
   );
 }
